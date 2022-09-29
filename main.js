e7d8099d4ea3fb77a0b23d67908e7a8bb7ba7560
Fercho612 <fernandopereyra4298@hotmail.com>
2022-09-29 11:00:36 -0300

    29/9 1hs

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const validateEmail = (email) => {
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
 };
+const validateMensj = (msj) => {
+  return msj.length > 10;
+}
 
-const validate = () => {
-  const $result = $('#result');
+const validateMail = () => {
+  const $result = $('#validMail');
   const email = $('#email').val();
   $result.text('');
 
@@ -18,6 +21,40 @@ const validate = () => {
   }
   return false;
 }
+const validateMsj = () => {
+  const $result = $('#validMsj');
+  const msj = $('#msj').val();
+  $result.text('');
+  
+  if(validateMensj(msj) == false) {
+    $result.text('Mensaje muy corto, digite minimo 10 caracteres');
+    $result.css('color', '#f55');
+  }
+}
+
+const sendMsj = () => {
+  const result = document.getElementById('validSend');
+  const email = $('#email').val();
+  const msj = $('#msj').val();
+  result = "";
+
+  if(validateEmail(email) == false) {
+    result += `Mail no valido <br>`;
+    return false;
+  } 
+  if(validateMensj(msj) == false) { 
+    result += 'Mensaje no valido';
+    return false;
+  }
+  if(validateMensj(msj) == true & validateEmail(email) == true) {
+    alert("Mensaje enviado")
+    return true;
+  }
+}
 
 
-$('#email').on('input', validate);
+
+$('#email').on('input', validateMail);
+$('#msj').on('input', validateMsj);
+
+
